b29c87aae16d2df7f4330224f77d17359068a16f
Kabir <kartiksahariya786@gmail.com>
2026-02-23 10:59:09 +0530

    Add files via upload

---
+++ 36_Excercise3_Tell_me_a_joke.js
@@ -0,0 +1,3 @@
+/* Element.innerHTML is used to populate a div with HTML
+Search online about this method and create a website with a div tag containing a random joke given an array
+of jokes. Use Math.random and fetch jokes from the internet (use any website to create the array) Your website should show a random joke on every reload Min length of your jokes array should be 10. */
